bfef968408ee5c33536439625508cec296e4ac62
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-05-09 14:23:16 +0530

    Adding Templating using EJs in list.ejs

--- app.js
+++ app.js
@@ -2,8 +2,42 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Server is up and running');
+  const today = new Date();
+  const currentDay = today.getDay();
+  let day = '';
+
+  switch (currentDay) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+
+    default:
+      console.log(`Error: Current day is equal to: ${currentDay}`);
+      break;
+  }
+
+  res.render('list', { kindOfDay: day });
 });
 
 const PORT = process.env.PORT || 3000;
