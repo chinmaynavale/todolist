d1adf87c068b07168a8dbb5081dc29cdfdf53bc2
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-05-09 23:10:34 +0530

    Implementing new feature- adding new todo item to a list

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
 const express = require('express');
-
 const app = express();
+
+let items = [];
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   const today = new Date();
-  const currentDay = today.getDay();
-  let day = '';
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
+  let day = today.toLocaleDateString('en-US', options);
 
-    default:
-      console.log(`Error: Current day is equal to: ${currentDay}`);
-      break;
-  }
+  res.render('list', {
+    kindOfDay: day,
+    newListItems: items,
+  });
+});
 
-  res.render('list', { kindOfDay: day });
+app.post('/', (req, res) => {
+  items.push(req.body.newItem);
+
+  res.redirect('/');
 });
 
 const PORT = process.env.PORT || 3000;
