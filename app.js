3eb15d1e1707aea0563b91953f1d426f93befbfe
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-05-23 17:35:36 +0530

    Initiating our app to work with MongoDB databases.

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require('express');
-const app = express();
-
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 
-app.set('view engine', 'ejs');
+const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const items = ['Buy food', 'Cook food', 'Eat food'];
 const workItems = [];
+
+mongoose
+  .connect('mongodb://localhost:27017/todolistDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Success!, Server is connected to database'))
+  .catch(err => console.error(err));
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: 'Welcome to your todolist!',
+});
+
+const item2 = new Item({
+  name: 'Hit the + button to add a new todo item.',
+});
+
+const item3 = new Item({
+  name: 'Click on any todo to mark it as complete.',
+});
+
+Item.insertMany([item1, item2, item3], err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Success!, saved default items to DB.');
+});
 
 // Home Page
 app.get('/', (req, res) => {
